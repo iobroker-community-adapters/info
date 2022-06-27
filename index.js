64e4348f8274c92b628721ca3d568209f01e26d2
Matthias Kleine <github.com9@emailu.de>
2022-06-27 10:44:18 +0200

    check-npm-permissions

--- index.js
+++ index.js
@@ -6,39 +6,46 @@ const fs = require('fs');
 
 let maintainers = JSON.parse(fs.readFileSync('maintainers.json'));
 
-let adapterList = [];
+if (process.argv.indexOf('--update-readme') > -1) {
+    console.log('Updating README.md');
 
-utils.collectRepos(adapterList)
-    .then(adapterList => {
-        let outPutTable = [];
+    utils.collectRepos()
+        .then(adapterList => {
+            let outPutTable = [];
 
-        outPutTable.push(['Adapter', 'Beta', 'Stable', 'Installed', 'Maintainer']);
+            outPutTable.push(['Adapter', 'Beta', 'Stable', 'Installed', 'Maintainer']);
 
-        for (const a in adapterList) {
-            const adapter = adapterList[a];
+            for (const a in adapterList) {
+                const adapter = adapterList[a];
 
-            const adapterName = adapter.name.replace('ioBroker.', '').toLowerCase();
-            const packageName = adapter.name.toLowerCase();
+                const adapterName = adapter.name.replace('ioBroker.', '').toLowerCase();
+                const packageName = adapter.name.toLowerCase();
 
-            outPutTable.push([
-                `[${adapter.name}](${adapter.html_url})`,
-                `![Beta](https://img.shields.io/npm/v/${packageName}.svg?color=red&label=beta)`,
-                `![Stable](http://iobroker.live/badges/${adapterName}-stable.svg)`,
-                `![Installed](http://iobroker.live/badges/${adapterName}-installed.svg)`,
-                maintainers?.[adapter.name] ? maintainers[adapter.name].map(m => `[${m}](https://github.com/${m}/)`).join(', ') : `-`
-            ]);
-        }
+                outPutTable.push([
+                    `[${adapter.name}](${adapter.html_url})`,
+                    `![Beta](https://img.shields.io/npm/v/${packageName}.svg?color=red&label=beta)`,
+                    `![Stable](http://iobroker.live/badges/${adapterName}-stable.svg)`,
+                    `![Installed](http://iobroker.live/badges/${adapterName}-installed.svg)`,
+                    maintainers?.[adapter.name] ? maintainers[adapter.name].map(m => `[${m}](https://github.com/${m}/)`).join(', ') : `-`
+                ]);
+            }
 
-        if (outPutTable.length > 0) {
-            // Update README
-            let readmeFileContents = String(fs.readFileSync('README.md'));
+            if (outPutTable.length > 0) {
+                // Update README
+                let readmeFileContents = String(fs.readFileSync('README.md'));
 
-            // Remove old markdown table and append new table
-            readmeFileContents = readmeFileContents.substring(0, readmeFileContents.indexOf('| Adapter'));
-            readmeFileContents += mdTable(outPutTable);
+                // Remove old markdown table and append new table
+                readmeFileContents = readmeFileContents.substring(0, readmeFileContents.indexOf('| Adapter'));
+                readmeFileContents += mdTable(outPutTable);
 
-            fs.writeFileSync('README.md', readmeFileContents);
+                fs.writeFileSync('README.md', readmeFileContents);
 
-            console.log('done.');
-        }
-    });
+                console.log('done.');
+            }
+        });
+} else if (process.argv.indexOf('--check-npm-permissions') > -1) {
+    utils.collectRepos()
+        .then(adapterList => {
+            // TODO
+        });
+}
