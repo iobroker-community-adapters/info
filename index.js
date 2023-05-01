de5575b4a3d9df02e706ca18f49e401830abd662
mcm1957 <mcm57@gmx.at>
2023-05-01 09:30:15 +0200

    add teststatus info

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ if (process.argv.indexOf('--update-readme') > -1) {
         .then(adapterList => {
             let outPutTable = [];
 
-            outPutTable.push(['Adapter', 'Beta', 'Stable', 'Installed', 'Maintainer']);
+            outPutTable.push(['Adapter', 'Beta', 'Stable', 'Installed', 'Teststatus', 'Maintainer']);
 
             for (const a in adapterList) {
                 const adapter = adapterList[a];
@@ -26,6 +26,7 @@ if (process.argv.indexOf('--update-readme') > -1) {
                     `![Beta](https://img.shields.io/npm/v/${packageName}.svg?color=red&label=beta)`,
                     `![Stable](http://iobroker.live/badges/${adapterName}-stable.svg)`,
                     `![Installed](http://iobroker.live/badges/${adapterName}-installed.svg)`,
+			  `![Test&Release](https://github.com/iobroker-community-adapters/iobroker.${adapterName}/workflows/Test%20and%20Release/badge.svg)`,
                     maintainers?.[adapter.name] ? maintainers[adapter.name].map(m => `[${m}](https://github.com/${m}/)`).join(', ') : `-`
                 ]);
             }
