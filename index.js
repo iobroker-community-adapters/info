a9567f30aa034013cd22d07852b9662f8911c2b4
Matthias Kleine <github.com9@emailu.de>
2023-11-10 08:38:54 +0100

    Use mustache to generate Readme (via GitHub actions)

--- index.js
+++ index.js
@@ -1,53 +1,56 @@
 'use strict';
 
-const mdTable = require('markdown-table');
+const Mustache = require('mustache');
 const utils = require('./lib/utils');
-const fs = require('fs');
+const fs = require('node:fs');
 
-let maintainers = JSON.parse(fs.readFileSync('maintainers.json'));
+const MUSTACHE_TEMPLATE = './README.mustache';
+const maintainers = JSON.parse(fs.readFileSync('maintainers.json'));
 
 if (process.argv.indexOf('--update-readme') > -1) {
     console.log('Updating README.md');
 
     utils.collectRepos()
         .then(adapterList => {
-            let outPutTable = [];
+            let templateData = {
+                generatedAt: new Date().toISOString(),
+                adapters: [],
+            };
 
-            outPutTable.push(['Adapter', 'Beta', 'Stable', 'Installed', 'Teststatus', 'Maintainer']);
-
-            for (const a in adapterList) {
-                const adapter = adapterList[a];
-                console.log ('    processing adapter '+adapter.name+' ('+a+')');
+            for (const adapter of adapterList) {
+                console.log (`    processing adapter ${adapter.name}`);
                 const adapterName = adapter.name.replace('ioBroker.', '').toLowerCase();
                 const packageName = adapter.name.toLowerCase();
 
                 let maint = '-';
-                if ( maintainers?.[adapter.name] === '!' )
+                if (maintainers?.[adapter.name] === '!') {
                     maint = `![Need Maintanance](https://img.shields.io/badge/needs-MAINTENANCE-red)`;
-                else if ( maintainers[adapter.name]=='DEPRECATED' )
+                } else if (maintainers[adapter.name] === 'DEPRECATED') {
                     maint = `![DEPRECATED](https://img.shields.io/badge/DEPRECATED-black)`;
-                else if ( maintainers?.[adapter.name] )
+                } else if (maintainers?.[adapter.name]) {
                     maint = maintainers[adapter.name].map(m => `[${m}](https://github.com/${m}/)`).join(', ');
+                }
 
-                outPutTable.push([
-                    `[${adapter.name}](${adapter.html_url})`,
-                    `![Beta](https://img.shields.io/npm/v/${packageName}.svg?color=red&label=beta)`,
-                    `![Stable](http://iobroker.live/badges/${adapterName}-stable.svg)`,
-                    `![Installed](http://iobroker.live/badges/${adapterName}-installed.svg)`,
-                    `![Test&Release](https://github.com/iobroker-community-adapters/iobroker.${adapterName}/workflows/Test%20and%20Release/badge.svg)`,
-                    `${maint}`
-                ]);
+                templateData.adapters.push({
+                    name: adapter.name,
+                    url: adapter.html_url,
+                    adapterName: adapterName,
+                    packageName: packageName,
+                    maintainer: maint,
+                });
             }
 
-            if (outPutTable.length > 0) {
-                // Update README
-                let readmeFileContents = String(fs.readFileSync('README.md'));
+            if (templateData.adapters.length > 0) {
+                try {
+                    // Update README
+                    const template = fs.readFileSync(MUSTACHE_TEMPLATE);
+                    const output = Mustache.render(template.toString(), templateData);
+                    fs.writeFileSync('README.md', output);
 
-                // Remove old markdown table and append new table
-                readmeFileContents = readmeFileContents.substring(0, readmeFileContents.indexOf('| Adapter'));
-                readmeFileContents += mdTable(outPutTable);
-
-                fs.writeFileSync('README.md', readmeFileContents);
+                    console.log('generated README...');
+                } catch (err) {
+                    console.error(`Unable to render mustache file "${MUSTACHE_TEMPLATE}": ${err}`);
+                }
 
                 console.log('done.');
             }
